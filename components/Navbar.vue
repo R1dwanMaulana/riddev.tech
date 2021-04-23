0cbbd70c0be59220601dd78497a3017bf8d802b1
Ridwan Maulana <rdwanf49@gmail.com>
2021-04-23 09:18:58 +0700

    fix: build reload

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
     <header class="lg:px-16 px-6 bg-white flex flex-wrap items-center lg:py-0 py-2 mb-20 mt-8">
     <div class="flex-1 flex justify-between items-center">
-      <img class="rounded-full w-10 h-10 border-2 border-transparent hover:border-indigo-400" src="../static/IMG_3118.jpg">
+      <img class="rounded-full w-10 h-10 border-2 border-transparent hover:border-indigo-400" src="../static/profile.jpg">
     </div>
 
    <label for="menu-toggle" class="cursor-pointer lg:hidden block"><svg class="fill-current text-gray-900" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><title>menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path></svg></label>
