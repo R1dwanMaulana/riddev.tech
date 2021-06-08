abc817551daad365a2929ef24bdebcde17975002
Ridwan Maulana <rdwanf49@gmail.com>
2021-06-09 06:59:36 +0700

    fix: improve design

--- pages/project.vue
+++ pages/project.vue
@@ -1,8 +1,8 @@
 <template>
 <div class="container p-5">
-    <div class="grid grid-cols-1 lg:max-w-4xl mx-auto sm:max-w-md md:max-w-2xl">
-        <!-- <h1 class="title">
-            My Programming Stack
+    <div class="lg:max-w-4xl mx-auto sm:max-w-md md:max-w-2xl">
+        <h1 class="title">
+            My Skill
         </h1>
 
         <p class="desc mb-3 text-gray-800">
@@ -38,37 +38,134 @@
                     </div>
                 </div>
             </div>
-        </div> -->
+        </div>
 
-        <h1 class="title mb-8">
-            All Projects
-        </h1>
-        <div class="max-w-sm rounded-md overflow-hidden hover:shadow-xl border border-gray-300 m-3">
-            <img class="w-full" src="../static/hijabstore.png" alt="Hijab Store">
-            <div class="px-6 py-4">
-                <div class="font-bold text-xl mb-2">Hijab Store</div>
-                <p class="text-gray-700 text-base">
-                build a hijab shop website with a user-friendly appearance.
-                </p>
+            <h1 class="title mt-10 mb-2">All Projects</h1>
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8">
+                    <div class="wrapper">
+                        <div class="card max-w-md rounded-lg overflow-hidden hover:shadow-xl border border-gray-300 transition-transform transform hover:scale-105 h-80">
+                            <img class="w-full" src="../static/restodev.png" alt="Web App Restoran">
+
+                            <div class="overlay info py-4 text-center">
+                                <div class="text-gray-700 font-bold text-xl">Restaurant App</div>
+                                <p class="text-gray-700 text-base p-2">
+                                restaurant search web app
+                                </p>
+                                <div class="px-6 pt-4 pb-2">
+                                    <p class="font-light text-sm mb-5"><span class="text-blue-700"><a href="https://github.com/R1dwanMaulana/Restaurant-PWA-and-Optimized" target="_blank">github.com/r1dwanmaulana/Restaurant-PWA-and-Optimized</a></span></p>
+                                    <span class="inline-block bg-yellow-400 rounded-md px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">Javascript</span>
+                                    <span class="inline-block bg-blue-700 rounded-md px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">CSS</span>
+                                    <span class="inline-block bg-yellow-500 rounded-md px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">Firebase</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="wrapper">
+                        <div class="card max-w-md rounded-lg overflow-hidden hover:shadow-xl border border-gray-300 transition-transform transform hover:scale-105 h-80">
+                            <img class="w-full" src="../static/hijabstore.png" alt="e-commerce Hijab Store">
+
+                            <div class="overlay info py-4 text-center">
+                                <div class="text-gray-700 font-bold text-xl">Hijab Store</div>
+                                <p class="text-gray-700 text-base p-2">
+                                Web e-commerce hijab store from Indonesia
+                                </p>
+                                <div class="px-6 pt-4 pb-2">
+                                    <p class="font-light text-sm mb-5"><span class="text-blue-700"><a href="https://github.com/R1dwanMaulana/Hijab-Store" target="_blank">github.com/r1dwanmaulana/Hijab-Store</a></span></p>
+                                    <span class="inline-block bg-green-500 rounded-md px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">VueJS</span>
+                                    <span class="inline-block bg-blue-700 rounded-md px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">CSS</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="wrapper">
+                        <div class="card max-w-md rounded-lg overflow-hidden hover:shadow-xl border border-gray-300 transition-transform transform hover:scale-105 h-80">
+                            <img class="w-full" src="../static/quran-id.png" alt="quran-id">
+
+                            <div class="overlay info py-4 text-center">
+                                <div class="text-gray-700 font-bold text-xl">Qur'an-id App</div>
+                                <p class="text-gray-700 text-base p-2">
+                                Al-Qur'an application from web browser + support install
+                                </p>
+                                <div class="px-6 pt-4 pb-2">
+                                    <p class="font-light text-sm mb-5"><span class="text-blue-700"><a href="https://github.com/R1dwanMaulana/quran-id" target="_blank">github/com/r1dwanmaulana/quran-id</a></span></p>
+                                    <span class="inline-block bg-green-500 rounded-md px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">NuxtJS</span>
+                                    <span class="inline-block bg-blue-500 rounded-md px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">TailwindCSS</span>
+                                    <span class="inline-block bg-gray-500 rounded-md px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">Vercel</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="wrapper">
+                        <div class="card max-w-md rounded-lg overflow-hidden hover:shadow-xl border border-gray-300 transition-transform transform hover:scale-105 h-80">
+                            <img class="w-full" src="../static/info-liga.png" alt="info-liga">
+
+                            <div class="overlay info py-4 text-center">
+                                <div class="text-gray-700 font-bold text-xl">Football league information</div>
+                                <p class="text-gray-700 text-base p-2">
+                                Football match information
+                                </p>
+                                <div class="px-6 pt-4 pb-2">
+                                    <p class="font-light text-sm mb-5"><span class="text-blue-700"><a href="https://github.com/R1dwanMaulana/information-football-PWA" target="_blank">github/com/r1dwanmaulana/information-football-PWA</a></span></p>
+                                    <span class="inline-block bg-yellow-400 rounded-md px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">Javascript</span>
+                                    <span class="inline-block bg-blue-700 rounded-md px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">CSS</span>
+                                    <span class="inline-block bg-gray-500 rounded-md px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">Vercel</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="wrapper">
+                        <div class="card max-w-md rounded-lg overflow-hidden hover:shadow-xl border border-gray-300 transition-transform transform hover:scale-105 h-80">
+                            <img class="w-full" src="../static/predictkos.png" alt="prediksi kos">
+
+                            <div class="overlay info py-4 text-center">
+                                <div class="text-gray-700 font-bold text-xl">Kos price prediction</div>
+                                <p class="text-gray-700 text-base p-2">
+                                Kos price prediction for JATIM area
+                                </p>
+                                <div class="px-6 pt-4 pb-2">
+                                    <p class="font-light text-sm mb-5"><span class="text-blue-700"><a href="https://github.com/R1dwanMaulana/prediksi-harga-kos" target="_blank">github/com/r1dwanmaulana/prediksi-harga-kos</a></span></p>
+                                    <span class="inline-block bg-green-500 rounded-md px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">NuxtJS</span>
+                                    <span class="inline-block bg-blue-500 rounded-md px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">TailwindCSS</span>
+                                    <span class="inline-block bg-gray-500 rounded-md px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">Vercel</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
             </div>
-            <div class="px-6 pt-4 pb-2">
-                <span class="inline-block bg-green-400 rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">Vuejs</span>
-                <span class="inline-block bg-blue-700 rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">CSS</span>
-            </div>
-        </div>
-        <div class="max-w-sm rounded-md overflow-hidden hover:shadow-xl border border-gray-300 m-3">
-            <img class="w-full" src="../static/restodev.png" alt="Web App Restoran">
-            <div class="px-6 py-4">
-                <div class="font-bold text-xl mb-2">Restoran</div>
-                <p class="text-gray-700 text-base">
-                building a restaurant search website equipped with a menu of available foods.
-                </p>
-            </div>
-            <div class="px-6 pt-4 pb-2">
-                <span class="inline-block bg-yellow-400 rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">Javascript</span>
-                <span class="inline-block bg-blue-600 rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">Webpack</span>
-            </div>
-        </div>
     </div>
 </div>
 </template>
+
+<style scoped>
+
+.card:hover img{
+    transform: translateY(-7px);
+    filter: blur(10px);
+}
+
+.card img {
+    height: 100%;
+    object-fit: cover;
+}
+.info {
+    position: absolute;
+    top: 20%;
+    /* overflow: hidden; */
+}
+.overlay {
+    opacity: 0
+}
+.wrapper:hover img {
+    opacity: 0.6
+} 
+.wrapper:hover .overlay {
+    opacity: 1;
+}
+
+</style>
