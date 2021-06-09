10a7773e74bdb43a6ebfcebad355d6539a77b220
Ridwan Maulana <rdwanf49@gmail.com>
2021-06-09 15:13:09 +0700

    improve design

--- pages/about.vue
+++ pages/about.vue
@@ -1,16 +1,5 @@
 <template>
     <div class="container p-5">
-        <!-- <div> -->
-            <!-- <h1 class="title text-center">
-                Contact Me👌🏻
-            </h1>
-
-            <h4 class="desc mb-3 text-gray-800 border border-gray-300 py-6 px-6 hover:shadow-xl border-red-400 rounded-lg">
-                Email : rdwanf49@gmail.com <br/>
-                Github : 🔗<a href="https://github.com/R1dwanMaulana" target="_blank"><strong>R1dwan Maulana</strong></a> <br/>
-                Linkedln : 🔗<a href="https://www.linkedin.com/in/ridwanms/" target="_blank"><strong>Ridwan Maulana</strong></a>
-            </h4> -->
-        <!-- </div> -->
         <div class="lg:max-w-4xl mx-auto sm:max-w-md md:max-w-2xl">
             <div class="md:grid lg:grid grid-flow-col grid-cols-4 grid-rows-1 gap-8">
                 <div>
