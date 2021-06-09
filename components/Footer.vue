32fd61e89d0ffcb1c876bfce9ac5408238a97b63
Ridwan Maulana <rdwanf49@gmail.com>
2021-06-10 06:16:05 +0700

    fix grid layout

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="flex flex-col">
-      <footer class="py-5 bg-white text-gray-500 text-center mb-5">
+      <footer class="py-5 bg-white text-gray-500 text-center mb-5 pr-4 pl-4 pt-20">
         <div class="text-center">
-            <p class="font-light text-sm">Contents licensed under <span class="text-blue-700"><a href="https://creativecommons.org/licenses/by-nc-sa/4.0/deed.id" target="_blank">CC BY-NC-SA 4.0.</a></span></p>
-            <p class="font-light text-sm">Made using <span class="text-blue-700"><a href="https://nuxtjs.org/" target="_blank">Nuxt.js</a></span> and  <span class="text-blue-700"><a href="https://tailwindcss.com/" target="_blank">Tailwind CSS.</a></span> Hosted on <span class="text-blue-700"><a href="https://vercel.com">Vercel</a></span></p>
-            <p class="font-light mt-3 text-sm">MIT License © 2020 - present | <span class="text-blue-700"><a href="https://riddev.tech" target="_blank">Ridwan Maulana.</a></span></p>
+            <p class="font-light text-xs">Contents licensed under <span class="text-blue-700"><a href="https://creativecommons.org/licenses/by-nc-sa/4.0/deed.id" target="_blank">CC BY-NC-SA 4.0.</a></span></p>
+            <p class="font-light text-xs">Made using <span class="text-blue-700"><a href="https://nuxtjs.org/" target="_blank">Nuxt.js</a></span> and  <span class="text-blue-700"><a href="https://tailwindcss.com/" target="_blank">Tailwind CSS.</a></span> Hosted on <span class="text-blue-700"><a href="https://vercel.com">Vercel</a></span></p>
+            <p class="font-light mt-3 text-xs">MIT License © 2020 - present | <span class="text-blue-700"><a href="https://riddev.tech" target="_blank">Ridwan Maulana.</a></span></p>
         </div>
       </footer>
   </div>
