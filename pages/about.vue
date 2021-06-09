fed1b7778c745c88bf11ba6e00f13646be945f3f
Ridwan Maulana <rdwanf49@gmail.com>
2021-06-09 09:11:56 +0700

    update: improve design

--- pages/about.vue
+++ pages/about.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container p-5">
-        <div>
-            <h1 class="title text-center">
+        <!-- <div> -->
+            <!-- <h1 class="title text-center">
                 Contact Me👌🏻
             </h1>
 
@@ -9,7 +9,55 @@
                 Email : rdwanf49@gmail.com <br/>
                 Github : 🔗<a href="https://github.com/R1dwanMaulana" target="_blank"><strong>R1dwan Maulana</strong></a> <br/>
                 Linkedln : 🔗<a href="https://www.linkedin.com/in/ridwanms/" target="_blank"><strong>Ridwan Maulana</strong></a>
-            </h4>
+            </h4> -->
+        <!-- </div> -->
+        <div class="lg:max-w-4xl mx-auto sm:max-w-md md:max-w-2xl">
+            <div class="md:grid lg:grid grid-flow-col grid-cols-4 grid-rows-1 gap-8">
+                <div>
+                    <p class="title mt-3">Languages</p>
+                    <ul>
+                        <li>HTML & CSS</li>
+                        <li>JavaScript</li>
+                        <li>PHP</li>
+                        <li>TypeScript</li>
+                    </ul>
+                </div>
+                <div>
+                    <p class="title mt-3">Frontend</p>
+                    <ul>
+                        <li>VueJS</li>
+                        <li>NuxtJS</li>
+                        <li>Tailwind CSS</li>
+                    </ul>
+                </div>
+                <div>
+                    <p class="title mt-3">Backend</p>
+                    <ul>
+                        <li>Firebase</li>
+                        <li>Node.JS</li>
+                        <li>Laravel</li>
+                    </ul>
+                </div>
+                <div>
+                    <p class="title mt-3">Infrastructure</p>
+                    <ul>
+                        <li>Google Cloud</li>
+                        <li>AWS</li>
+                        <li>Vercel</li>
+                        <li>Netlify</li>
+                    </ul>
+                </div>
+            </div>
+
+            <h1 class="title mt-5">
+                Contact Me👌🏻
+            </h1>
+                <p>You can contact via email at <span class="text-red-500"><a href=" mailto:rdwanf49@gmail.com" target="_blank">rdwanf49@gmail.com</a></span>, or through the social media listed below :</p>
+                <ul>
+                    <li>Github : <span class="text-red-500 text-base"><a href="https://github.com/R1dwanMaulana" target="_blank">r1dwanmaulana</a></span></li>
+                    <li>Linkedln : <span class="text-red-500 text-base"><a href="https://www.linkedin.com/in/ridwanms/" target="_blank">ridwanms</a></span></li>
+                    <li>Twitter : <span class="text-red-500 text-base"><a href="https://www.twitter.com/in/ridvisible/" target="_blank">ridvisible</a></span></li>
+                </ul>
         </div>
     </div>
 </template>
