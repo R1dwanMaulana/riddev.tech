6000f1d916cb9b9fae8d20876805eb7deccb4a3c
Ridwan Maulana <rdwanf49@gmail.com>
2021-09-10 18:39:08 +0700

    feat: improve nav link

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -4,7 +4,9 @@
       <img class="rounded-full w-10 h-10 border-2 border-transparent hover:border-indigo-400" src="../static/profile.jpg">
     </div>
 
-   <label for="menu-toggle" class="cursor-pointer lg:hidden block"><svg class="fill-current text-gray-900" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><title>menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path></svg></label>
+  <button>
+        <label for="menu-toggle" class="cursor-pointer lg:hidden block focus:outline-none"><img :src="toggleIcon" @click="menuIcon = !menuIcon" class="hover:bg-gray-200 rounded-sm"></label>
+  </button>
   <input class="hidden" type="checkbox" id="menu-toggle" />
 
   <div class="hidden lg:flex lg:items-center lg:w-auto w-full" id="menu">
@@ -16,14 +18,35 @@
         <li><nuxt-link class="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-indigo-400" to="/about">About</nuxt-link></li>
       </ul>
     </nav>
-
   </div>
 
   </header>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      menuIcon: false
+    }
+  },
+  computed: {
+    toggleIcon() {
+        if(this.menuIcon) {
+            return require('../static/close.svg')
+        } else {
+            return require('../static/menu.svg')
+        }
+    }
+  }
+}
+</script>
+
 <style scoped>
       #menu-toggle:checked + #menu {
         display: block;
+      }
+      .nuxt-link-exact-active {
+        color: #2563EB;
       }
 </style>
