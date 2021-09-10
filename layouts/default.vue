74f5fb10b31372243289172b6b1aa830eb6aa67f
Ridwan Maulana <rdwanf49@gmail.com>
2021-09-10 08:21:27 +0700

    chore: remove chatbox

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,19 +6,6 @@
   </div>
 </template>
 
-<script>
-export default {
-  data() {
-    function initFreshChat() {
-      window.fcWidget.init({
-        token: "6bf0b6f1-0488-46ba-ac45-f6ac52abfaa8",
-        host: "https://wchat.freshchat.com"
-      });
-    }
-    function initialize(i,t){var e;i.getElementById(t)?initFreshChat():((e=i.createElement("script")).id=t,e.async=!0,e.src="https://wchat.freshchat.com/js/widget.js",e.onload=initFreshChat,i.head.appendChild(e))}function initiateCall(){initialize(document,"Freshchat-js-sdk")}window.addEventListener?window.addEventListener("load",initiateCall,!1):window.attachEvent("load",initiateCall,!1);
-  }
-}
-</script>
 
 <style>
 
